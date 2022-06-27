df3a4484e60aaf9cfbb0df9a96a308c61af02110
Aniket Chaudhri <anichaudhri@gmail.com>
2022-06-28 01:22:32 +0530

    Add signup router

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
+import SignUp from './user/SignUp';
 
 const MainRouter = () => (
     <div>
         <Switch>
-            <Route path="/" component={Home} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={SignUp} />
         </Switch>
     </div>
 )
