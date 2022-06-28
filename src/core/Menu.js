44e8973f493fe1c8d4bf62d9db85f532a0b5338f
Aniket Chaudhri <anichaudhri@gmail.com>
2022-06-28 22:04:45 +0530

    Add SignOut

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -6,14 +6,31 @@ const isActive = (history, path) => {
     // if (history.location.pathname === path) {
     //     return {color: "#ff9900"};
     // }
-    return {color: "#00fff8"};
+    return { color: "#00fff8" };
 }
 
-const Menu = ({history}) => (
+export const signout = (next) => {
+    if (typeof window !== 'undefined') {
+        localStorage.removeItem('jwt');
+    }
+    next();
+    return fetch('http://localhost:8080/signout', {
+        method: 'GET',
+
+    })
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .catch(error => console.error('Error:', error))
+}
+
+
+const Menu = ({ history }) => (
     <div>
         <ul className="nav nav-tabs mb-3 bg-primary" id="ex1" role="tablist">
             <li className="nav-item" role="presentation">
-                <Link className="nav-link" style = {isActive(history, "/")} to="/">Home</Link>
+                <Link className="nav-link" style={isActive(history, "/")} to="/">Home</Link>
             </li>
             <li className="nav-item" role="presentation">
                 <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Sign Up</Link>
@@ -21,9 +38,11 @@ const Menu = ({history}) => (
             <li className="nav-item" role="presentation">
                 <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">Sign In</Link>
             </li>
-
+            <li className="nav-item" role="presentation">
+                <a className="nav-link" style={(isActive(history, "/signin"), {cursor: "pointer", color: "#fff"})} onClick={() => signout(() => history.push('/'))}>Sign Out</a>
+            </li>
         </ul>
-        
+
         {/* {()=>console.log(JSON.stringify(props))} */}
     </div>
 )
