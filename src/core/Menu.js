fc7d2c730342e83774fc4281c0d4698c3bfa82df
Aniket Chaudhri <anichaudhri@gmail.com>
2022-06-28 21:48:06 +0530

    Add Menu NavBar

---
+++ src/core/Menu.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+
+const isActive = (history, path) => {
+    // if (history.location.pathname === path) {
+    //     return {color: "#ff9900"};
+    // }
+    return {color: "#00fff8"};
+}
+
+const Menu = ({history}) => (
+    <div>
+        <ul className="nav nav-tabs mb-3 bg-primary" id="ex1" role="tablist">
+            <li className="nav-item" role="presentation">
+                <Link className="nav-link" style = {isActive(history, "/")} to="/">Home</Link>
+            </li>
+            <li className="nav-item" role="presentation">
+                <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Sign Up</Link>
+            </li>
+            <li className="nav-item" role="presentation">
+                <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">Sign In</Link>
+            </li>
+
+        </ul>
+        
+        {/* {()=>console.log(JSON.stringify(props))} */}
+    </div>
+)
+
+export default withRouter(Menu);
